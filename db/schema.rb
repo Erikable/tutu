9fedba2be65b6e7bf6e76d2adef2c65694484da8
Erikable <surmiloev@gmail.com>
2017-09-26 18:10:35 +0300

    route CRUD + valid

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925135114) do
+ActiveRecord::Schema.define(version: 20170926131411) do
 
   create_table "railway_stations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
