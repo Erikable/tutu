d34472ab6c19a67ea55978c2aa44179f6ecab136
Erikable <surmiloev@gmail.com>
2017-10-04 17:50:38 +0300

    re-сетик

--- db/migrate/20170929142443_create_users.rb
+++ db/migrate/20170929142443_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name
-      t.timestamps
+    t.string :name
+    t.timestamps
     end
   end
 end
