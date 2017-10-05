c16958141fdf03651d5f356332070ac881645a50
Erikable <surmiloev@gmail.com>
2017-10-05 04:39:39 +0300

    13ing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002212901) do
+ActiveRecord::Schema.define(version: 20171005005044) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -34,7 +34,13 @@ ActiveRecord::Schema.define(version: 20171002212901) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "train_id"
+    t.integer "user_id"
+    t.integer "start_station_id"
+    t.integer "end_station_id"
+    t.index ["end_station_id"], name: "index_tickets_on_end_station_id"
+    t.index ["start_station_id"], name: "index_tickets_on_start_station_id"
     t.index ["train_id"], name: "index_tickets_on_train_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "trains", force: :cascade do |t|
