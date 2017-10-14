a60ebc6ce106a3b86432cb84a1cf8582a3add163
Erikable <surmiloev@gmail.com>
2017-10-14 17:22:13 +0300

    2models changed+wag_dbmigrate

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,5 +2,6 @@ class Train < ApplicationRecord
   belongs_to :route, optional: true
   has_many :tickets
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, optional: true
+  has_many :wagons
   validates :number, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005005044) do
+ActiveRecord::Schema.define(version: 20171014074849) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 20171005005044) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "wagons", force: :cascade do |t|
+    t.string "wagon_type"
+    t.integer "top_seats"
+    t.integer "bottom_seats"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
