8576ef0677ef3d8becd42f5c1af1b65b8d06d08d
Erikable <surmiloev@gmail.com>
2017-10-16 01:29:53 +0300

    add_display-wagons-in_train

--- app/models/train.rb
+++ app/models/train.rb
@@ -4,4 +4,13 @@ class Train < ApplicationRecord
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, optional: true
   has_many :wagons
   validates :number, presence: true
+
+  def train_has_wagons(wag_type) # Кол-во плацкартный/купейных вагонов в составе поезда
+    self.wagons.count {|wagon| wagon.wagon_type == wag_type}
+  end
+
+  def seats_in_wagon(wag_type, side_type) # Кол-во мест: плацкартных верхних/нижних, купейных верхних/нижних
+    self.wagons.where(wagon_type: wag_type).sum(side_type)
+  end
+
 end
