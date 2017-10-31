2248d57c3a459168dff47ebb8628695de65c34c1
Erikable <surmiloev@gmail.com>
2017-11-01 01:31:31 +0300

    16lesson

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -3,11 +3,7 @@ class RailwayStation < ApplicationRecord
   has_many :routes, through: :railway_stations_routes
   has_many :trains, foreign_key: :current_station_id
 
-  # scope :sort_stations_asc, -> { order('station_number DESC') }
-  # scope :sorted, -> { joins(:railway_stations_routes).order(‘railway_stations_routes.station_number ASC’) }
-  
-  def set_station_number
-  
-  end
+  # Выводить список станций в маршруте по порядковому номеру
+  scope :sorted, -> { joins(:railway_stations_routes).order(‘railway_stations_routes.station_number ASC’) }
 
 end
--- app/models/train.rb
+++ app/models/train.rb
@@ -1,11 +1,18 @@
 class Train < ApplicationRecord
-  has_many :wagons
+  has_many :wagons, -> (train) {if train.flag 
+      order(number: :asc)
+    else 
+      order(number: :desc)
+    end
+  }
+
   belongs_to :route, optional: true
   has_many :tickets
   belongs_to :current_station, class_name: 'RailwayStation', foreign_key: :current_station_id, optional: true
   validates :number, presence: true
 
-  def sort_wagons
-    
+  def count_seats(wagon_type, seats_type)
+    wagons.where(type: wagon_type).sum(seats_type) 
   end
+
 end
