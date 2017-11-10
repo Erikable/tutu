fcadd560c0869b8f4bd9890d8e3aedae14f1e3bb
Erikable <surmiloev@gmail.com>
2017-11-10 18:53:38 +0300

    2nd_done16

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -4,7 +4,7 @@ class RailwayStation < ApplicationRecord
   has_many :trains, foreign_key: :current_station_id
 
   # Выводить список станций в маршруте по порядковому номеру
-  scope :sorted, -> { joins(:railway_stations_routes).order("railway_stations_routes.station_number ASC").uniq }
-  #scope :rrr, -> { select("railway_stations.*, railway_stations_routes.station_number") }
+  scope :sorted, -> { joins(:railway_stations_routes, :routes).order("railway_stations_routes.station_number ASC").uniq }
+  scope :rrr, -> { select("railway_stations.*, railway_stations_routes.station_number") }
 
 end
