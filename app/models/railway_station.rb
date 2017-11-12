31715f55550b8e6bcde3bdea70382909cdced350
Erikable <surmiloev@gmail.com>
2017-11-12 23:52:56 +0300

    перезалив подтвержденной домашки-16

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -3,7 +3,6 @@ class RailwayStation < ApplicationRecord
   has_many :routes, through: :railway_stations_routes
   has_many :trains, foreign_key: :current_station_id
 
-  # Выводить список станций в маршруте по порядковому номеру
   scope :sorted, -> { joins(:railway_stations_routes, :routes).order("railway_stations_routes.station_number ASC").uniq }
   
   scope :station_number_to_station, -> { select("railway_stations.*, railway_stations_routes.station_number") }
