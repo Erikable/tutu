8ef1d73d142966651b0e9677305deead43ad5e1a
Erikable <surmiloev@gmail.com>
2017-11-15 12:34:34 +0300

    сортировка из скринкаста

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -3,7 +3,23 @@ class RailwayStation < ApplicationRecord
   has_many :routes, through: :railway_stations_routes
   has_many :trains, foreign_key: :current_station_id
 
-  scope :sorted, -> { joins(:railway_stations_routes, :routes).order("railway_stations_routes.station_number ASC").uniq }
-  scope :station_number_to_station, -> { select("railway_stations.*, railway_stations_routes.station_number") }
+  #scope :sorted, -> { joins(:railway_stations_routes, :routes).order("railway_stations_routes.station_number ASC").uniq }
+  #scope :station_number_to_station, -> { select("railway_stations.*, railway_stations_routes.station_number") }
+  scope :ordered_station_number_to_station, -> { joins(:railway_stations_routes).order("railway_stations_routes.station_number").uniq }
+
+  def update_position(route, station_number)
+    station_route = railway_stations_routes.where(route: route).first
+    station_route.update(station_number: station_number) if station_route
+  end
+
+  def position_in(route)
+    station_route(route).try(:station_number)
+  end
+
+  protected
+
+  def station_route(route)
+    @station_route ||= railway_stations_routes.where(route: route).first
+  end
 
 end
