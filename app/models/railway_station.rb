c24ecb37e1401db9322c61ea390b9177f5951292
Erikable <surmiloev@gmail.com>
2017-11-18 01:58:11 +0300

    время прибытия и отправления -  не показ но в бд есть

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -3,8 +3,6 @@ class RailwayStation < ApplicationRecord
   has_many :routes, through: :railway_stations_routes
   has_many :trains, foreign_key: :current_station_id
 
-  #scope :sorted, -> { joins(:railway_stations_routes, :routes).order("railway_stations_routes.station_number ASC").uniq }
-  #scope :station_number_to_station, -> { select("railway_stations.*, railway_stations_routes.station_number") }
   scope :ordered_station_number_to_station, -> { joins(:railway_stations_routes).order("railway_stations_routes.station_number").uniq }
 
   def update_position(route, station_number)
@@ -14,6 +12,25 @@ class RailwayStation < ApplicationRecord
 
   def position_in(route)
     station_route(route).try(:station_number)
+  end
+
+  def update_arrival_time(route, arrival_time)
+    station_route ||= railway_stations_routes.where(route: route).first
+    station_route.update(arrival_time: arrival_time) if station_route
+  end
+
+  def arrival_at(route)
+    station_route(route).try(:arrival_time)
+  end
+
+
+  def update_departure_time(route, arrival_time)
+    station_route ||= railway_stations_routes.where(route: route).first
+    station_route.update(departure_time: departure_time) if station_route
+  end
+
+  def departure_at(route)
+    station_route(route).try(:departure_time)
   end
 
   protected
