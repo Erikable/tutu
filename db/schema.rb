e8bbd6fb78cbb2a316eb452fcdb559fa0d5c07cf
Erikable <surmiloev@gmail.com>
2017-11-18 00:09:14 +0300

    добавлены поля времени + шоу

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110100933) do
+ActiveRecord::Schema.define(version: 20171117151052) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20171110100933) do
     t.integer "railway_station_id"
     t.integer "route_id"
     t.integer "station_number"
+    t.datetime "departure_time"
+    t.datetime "arrival_time"
   end
 
   create_table "routes", force: :cascade do |t|
