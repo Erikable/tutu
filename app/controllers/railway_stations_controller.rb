e4f2433ea500d5fc8678d798118f434aa09d71b6
Erikable <surmiloev@gmail.com>
2017-12-17 02:16:21 +0300

    частичное изменение по замечанию..

--- app/controllers/railway_stations_controller.rb
+++ app/controllers/railway_stations_controller.rb
@@ -1,5 +1,6 @@
 class RailwayStationsController < ApplicationController
   before_action :set_railway_station, only: [:show, :edit, :update, :destroy, :update_position, :update_arrival_time, :update_departure_time]
+  before_action :set_route, only: [:update_position,:update_arrival_time,:update_departure_time]
 
   def index
     @railway_stations = RailwayStation.all
@@ -39,24 +40,25 @@ class RailwayStationsController < ApplicationController
   end
 
   def update_position
-    @route = Route.find(params[:route_id])
     @railway_station.update_position(@route, params[:station_number])
     redirect_to @route
   end
 
   def update_arrival_time
-    @route = Route.find(params[:route_id])
     @railway_station.update_arrival_time(@route, params[:arrival_time])
     redirect_to @route
   end
 #
   def update_departure_time
-    @route = Route.find(params[:route_id])
     @railway_station.update_departure_time(@route, params[:departure_time])
     redirect_to @route
   end
 
   private
+    def set_route
+      @route = Route.find(params[:route_id])
+    end
+
     def set_railway_station
       @railway_station = RailwayStation.find(params[:id])
     end
