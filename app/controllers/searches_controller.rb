309c0b06c7afd5e22a613e6f5130a886ac77549a
Erikable <surmiloev@gmail.com>
2017-12-16 14:08:50 +0300

    поиск на 1й стр

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,12 +1,20 @@
 class SearchesController < ApplicationController
- 
-  def new
-    @stations = RailwayStation.all
+  
+  before_action :set_stations, only: [:show, :create]
+  
+  def show
   end
 
-  def show
+  def create
     @start_station = RailwayStation.find(params[:start_station_id])
     @end_station = RailwayStation.find(params[:end_station_id])
     @routes = Route.search_route_with_stations(@start_station, @end_station)
+    render :show
+  end
+
+  private
+  
+  def set_stations
+    @stations = RailwayStation.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   end
   
   resources :routes
-  resource :search, only: [:new, :show, :edit]
+  resource :search, only: [:show, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'railway_stations#index'
