2afca60571c26bf228db68ba0d9659f177abdd28
Erikable <surmiloev@gmail.com>
2017-12-28 16:13:30 +0300

    19lssnStart

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -2,7 +2,7 @@ class RailwayStation < ApplicationRecord
   has_many :railway_stations_routes
   has_many :routes, through: :railway_stations_routes
   has_many :trains, foreign_key: :current_station_id
-
+  validates :title, presence: true
   scope :order_stations, -> { joins(:railway_stations_routes).order("railway_stations_routes.station_number").uniq }
 
   def update_position(route, station_number)
