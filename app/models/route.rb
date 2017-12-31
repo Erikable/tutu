cc8c1cd212993eed623d99053a8f257aadc6ea4c
Erikable <surmiloev@gmail.com>
2017-12-31 19:31:41 +0300

    starting_TBS

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,23 +4,22 @@ class Route < ApplicationRecord
   has_many :trains
 
   before_validation :set_title
-
+  
   validate :stations_count
   validates :title, presence: true
-
+ 
   def self.search_route_with_stations(start_station_id, end_station_id)
     joins(:railway_stations).where(railway_stations: { id: start_station_id }) & joins(:railway_stations).where(railway_stations: { id: end_station_id })
   end
 
   private
-
   def set_title
     self.title = "#{railway_stations.first.title} - #{railway_stations.last.title}"
   end
 
   def stations_count
     if railway_stations.size < 2
-      errors.add(:base, "Маршрут Должен содержать мин 2 станции")
+      errors.add(:base, "Маршрут Должен содержать 2 станции")
     end
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,4 +4,7 @@ class Ticket < ApplicationRecord
   belongs_to :start_station, class_name: 'RailwayStation', foreign_key: :start_station_id, optional: true
   belongs_to :end_station, class_name: 'RailwayStation', foreign_key: :end_station_id, optional: true
   validates :number, presence: true
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :passport, presence: true
 end
